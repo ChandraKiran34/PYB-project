849f510a1a6bc6230214ca44ae5997bf845601c6
ChandraKiran34 <bchakri06@gmail.com>
2024-02-22 23:48:48 +0530

    front-end

--- back-end/routes/TravellerAuthenticate.js
+++ back-end/routes/TravellerAuthenticate.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { registerTraveller, loginTraveller, updateTravellerDetailsById, getBookingDetailsByTravellerId, getTravellerDetails } from '../controllers/travellerController.js';
 import { verifyToken } from '../middleware/travelAuth.js';
-import { getBookingDetailsByHotelId, getHotelDetails, updateHotelDetailsById } from '../controllers/hotelController.js';
 const router = express.Router();
 
 // Register a new traveller
@@ -16,12 +15,12 @@ router.use(verifyToken);
 
 
 // Get traveller details by ID (dashboard)
-router.get('/:hotelId/dashboard', getHotelDetails);
+router.get('/:hotelId/dashboard', getTravellerDetails);
 
 // Update traveller details by ID
-router.put('/:hotelId/update', updateHotelDetailsById);
+router.put('/:hotelId/update', updateTravellerDetailsById);
 
 // Get booking details by traveller ID
-router.get('/:hotelId/bookings', getBookingDetailsByHotelId);
+router.get('/:hotelId/bookings', getBookingDetailsByTravellerId);
 
 export default router;
